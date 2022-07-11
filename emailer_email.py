5fa3245ef89e9f261aa0d43fa90f76d0761e0f81
Mofiro Jean <mofirojean@gmail.com>
2022-07-11 12:26:14 +0100

    feat: added a decouple module to work with the .env file

--- emailer_email.py
+++ emailer_email.py
@@ -1,3 +1,10 @@
 # deals with attaching the document to the email and sending of
 # emails to their various SMTP
 import smtplib
+from decouple import config
+
+
+# Retrieving our configuration requirement
+EMAIL_ADDRESS = config("EMAIL_ADDRESS")
+EMAIL_PASS = config("EMAIL_PASS")
+

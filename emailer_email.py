1185f85730d9b31eb29646ae71f8cd3d74bd785a
Mofiro Jean <mofirojean@gmail.com>
2022-08-01 19:30:25 +0100

    feat: minor changes

--- emailer_email.py
+++ emailer_email.py
@@ -8,3 +8,23 @@ from decouple import config
 EMAIL_ADDRESS = config("EMAIL_ADDRESS")
 EMAIL_PASS = config("EMAIL_PASS")
 
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    # encrypting our mail server
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    # login in to our gmail using environment variables
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASS)
+
+    # creating a simple meassage
+    subject = "Hello Mofiro"
+    body = "How are you doing"
+
+    msg = f"Subject: {subject}\n\n{body}"
+
+    # sending the email
+    smtp.sendmail(EMAIL_ADDRESS, "mofirojean@gmail.com", msg)
+
+
+

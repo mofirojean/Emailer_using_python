9b4c70ae179d25f5ea085feff7e9a3b47906238e
Mofiro Jean <mofirojean@gmail.com>
2022-09-07 14:24:15 +0100

    generates a word document from the template file and converts it to pdf

---
+++ emailer_docx_template.py
@@ -0,0 +1,30 @@
+﻿"""
+ ***********************************************
+ Generates a word document from the template file
+ ***********************************************
+"""
+from msoffice2pdf import convert
+from docxtpl import DocxTemplate
+
+
+class GenerateTemplate:
+    """Initialize parameters"""
+    def __init__(self, userName, companyName):
+        self.name = userName
+        self.companyName = companyName
+
+    # Generates the document and saves it in the Docs folder
+    def generateTemplate(self):
+        doc = DocxTemplate("./Template/Template.docx")
+        context = {'company': self.companyName, 'name': self.name}
+        doc.render(context)
+        doc.save(f"./Docs/{self.name}.docx")
+        output = convert(source=f"./Docs/{self.name}.docx", output_dir="./Docs_Pdf/", soft=1)
+        print(output)
+
+        return output
+
+
+# Driver Method
+generate = GenerateTemplate("mofiro", "NerdTech")
+generate.generateTemplate()

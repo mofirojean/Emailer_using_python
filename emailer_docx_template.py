5db509f7b201452d69ec7ef6ac861c01c98a22f1
Mofiro Jean <mofirojean@gmail.com>
2022-09-07 15:49:39 +0100

    feat: added a pdf converter and improve code to delete word document after being converted to pdf

--- emailer_docx_template.py
+++ emailer_docx_template.py
@@ -1,30 +1,33 @@
 ﻿"""
  ***********************************************
- Generates a word document from the template file
+ Generates a pdf document from the template file
  ***********************************************
 """
+import pathlib
 from msoffice2pdf import convert
 from docxtpl import DocxTemplate
 
 
 class GenerateTemplate:
     """Initialize parameters"""
-    def __init__(self, userName, companyName):
-        self.name = userName
+    def __init__(self, companyName):
         self.companyName = companyName
 
-    # Generates the document and saves it in the Docs folder
-    def generateTemplate(self):
+    # Generates the pdf document and saves it, in the Docs folder
+    def generateTemplate(self, userName):
+        """ Generates a Word document from the template file"""
         doc = DocxTemplate("./Template/Template.docx")
-        context = {'company': self.companyName, 'name': self.name}
+        context = {'company': self.companyName, 'name': userName}
         doc.render(context)
-        doc.save(f"./Docs/{self.name}.docx")
-        output = convert(source=f"./Docs/{self.name}.docx", output_dir="./Docs_Pdf/", soft=1)
-        print(output)
+        fileName = f"./Docs/{userName}.docx"
+        doc.save(fileName)
 
+        # converts to pdf
+        output = convert(source=fileName, output_dir="./Docs", soft=1)
+
+        # Deletes the word documented generated from the template
+        file = pathlib.Path(fileName)
+        file.unlink()
+
+        # Returns the path of the pdf file generated
         return output
-
-
-# Driver Method
-generate = GenerateTemplate("mofiro", "NerdTech")
-generate.generateTemplate()

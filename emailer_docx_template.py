6ec15f84f2b1b6e80231d7fb9e2a757388d0e1c9
mofirojean <mofirojean@gmail.com>
2022-10-30 17:28:18 +0100

    updated emailer_docx_template.py

--- emailer_docx_template.py
+++ emailer_docx_template.py
@@ -10,23 +10,23 @@ from docxtpl import DocxTemplate
 
 class GenerateTemplate:
     """Initialize parameters"""
-    def __init__(self, companyName):
-        self.companyName = companyName
+    def __init__(self, company_name):
+        self.companyName = company_name
 
     # Generates the pdf document and saves it, in the Docs folder
-    def generateTemplate(self, userName):
+    def generate_template(self, user_name):
         """ Generates a Word document from the template file"""
         doc = DocxTemplate("./Template/Template.docx")
-        context = {'company': self.companyName, 'name': userName}
+        context = {'company': self.companyName, 'name': user_name}
         doc.render(context)
-        fileName = f"./Docs/{userName}.docx"
-        doc.save(fileName)
+        file_name = f"./Docs/{user_name}.docx"
+        doc.save(file_name)
 
         # converts to pdf
-        output = convert(source=fileName, output_dir="./Docs", soft=1)
+        output = convert(source=file_name, output_dir="./Docs", soft=1)
 
         # Deletes the word documented generated from the template
-        file = pathlib.Path(fileName)
+        file = pathlib.Path(file_name)
         file.unlink()
 
         # Returns the path of the pdf file generated
